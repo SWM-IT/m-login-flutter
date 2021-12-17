ab751ad91ebf1f171ed70da1b0f68d7cc22b4a6b
Laura Mühlbach <lauramuhlbach@Lauras-MacBook-Pro.local>
2021-12-17 15:50:48 +0100

    make mlogin button accessible

--- lib/src/ui_elements/m_login_button.dart
+++ lib/src/ui_elements/m_login_button.dart
@@ -11,6 +11,7 @@ class MLoginButton extends StatelessWidget {
   final String text;
   final bool enabled;
   final EdgeInsets padding;
+  final String? semanticsLabel;
 
   const MLoginButton({
     Key? key,
@@ -18,68 +19,75 @@ class MLoginButton extends StatelessWidget {
     required this.text,
     this.enabled = true,
     this.padding = EdgeInsets.zero,
+    this.semanticsLabel,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 2.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
-      ),
-      clipBehavior: Clip.hardEdge,
-      color: Colors.transparent,
-      child: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Ink.image(
-            image: const AssetImage(
-              'lib/assets/m_login_button_bg.png',
-              package: 'm_login_sdk',
-            ),
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 40,
-            child: InkWell(
-              onTap: onPressed,
-              highlightColor: Platform.isAndroid
-                  ? Colors.white.withOpacity(0.2)
-                  : Colors.black.withOpacity(0.2),
-              splashColor: Platform.isAndroid
-                  ? Colors.white.withOpacity(0.2)
-                  : Colors.transparent,
-            ),
-          ),
-          IgnorePointer(
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 4, right: 15),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(2),
-                    child: Image.asset(
-                      'lib/assets/m_login_button_icon.png',
-                      package: 'm_login_sdk',
-                      height: 33,
-                    ),
-                  ),
-                ),
-                Text(
-                  text,
-                  style: const TextStyle(color: Colors.white, fontSize: 16),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-          ),
-          if (!enabled)
-            Container(
-              color: const Color.fromRGBO(220, 220, 220, 0.5),
+    return Semantics(
+      button: true,
+      label: semanticsLabel ?? text,
+      child: Material(
+        elevation: 2.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        clipBehavior: Clip.hardEdge,
+        color: Colors.transparent,
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Ink.image(
+              image: const AssetImage(
+                'lib/assets/m_login_button_bg.png',
+                package: 'm_login_sdk',
+              ),
+              fit: BoxFit.fitWidth,
               width: double.infinity,
               height: 40,
+              child: InkWell(
+                onTap: onPressed,
+                highlightColor: Platform.isAndroid
+                    ? Colors.white.withOpacity(0.2)
+                    : Colors.black.withOpacity(0.2),
+                splashColor: Platform.isAndroid
+                    ? Colors.white.withOpacity(0.2)
+                    : Colors.transparent,
+              ),
             ),
-        ],
+            IgnorePointer(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4, right: 15),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(2),
+                      child: Image.asset(
+                        'lib/assets/m_login_button_icon.png',
+                        package: 'm_login_sdk',
+                        height: 33,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      text,
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (!enabled)
+              Container(
+                color: const Color.fromRGBO(220, 220, 220, 0.5),
+                width: double.infinity,
+                height: 40,
+              ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/ui_elements/m_login_button_white.dart
+++ lib/src/ui_elements/m_login_button_white.dart
@@ -13,6 +13,7 @@ class MLoginButtonWhite extends StatelessWidget {
   final String text;
   final bool enabled;
   final EdgeInsets padding;
+  final String? semanticsLabel;
 
   const MLoginButtonWhite({
     Key? key,
@@ -20,64 +21,71 @@ class MLoginButtonWhite extends StatelessWidget {
     required this.text,
     this.enabled = true,
     this.padding = EdgeInsets.zero,
+    this.semanticsLabel,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 2.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
-      ),
-      clipBehavior: Clip.hardEdge,
-      color: Colors.transparent,
-      child: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Ink(
-            color: Colors.white,
-            width: double.infinity,
-            height: 40,
-            child: InkWell(
-              onTap: onPressed,
-              highlightColor: Platform.isAndroid
-                  ? mLoginBlue.withOpacity(0.1)
-                  : Colors.black.withOpacity(0.1),
-              splashColor: Platform.isAndroid
-                  ? mLoginBlue.withOpacity(0.2)
-                  : Colors.transparent,
-            ),
-          ),
-          IgnorePointer(
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 4, right: 15),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(2),
-                    child: Image.asset(
-                      'lib/assets/m_login_button_white_icon.png',
-                      package: 'm_login_sdk',
-                      height: 33,
-                    ),
-                  ),
-                ),
-                Text(
-                  text,
-                  style: const TextStyle(color: mLoginBlue, fontSize: 16),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-          ),
-          if (!enabled)
-            Container(
-              color: const Color.fromRGBO(210, 210, 210, 0.5),
+    return Semantics(
+      button: true,
+      label: semanticsLabel ?? text,
+      child: Material(
+        elevation: 2.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        clipBehavior: Clip.hardEdge,
+        color: Colors.transparent,
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Ink(
+              color: Colors.white,
               width: double.infinity,
               height: 40,
+              child: InkWell(
+                onTap: onPressed,
+                highlightColor: Platform.isAndroid
+                    ? mLoginBlue.withOpacity(0.1)
+                    : Colors.black.withOpacity(0.1),
+                splashColor: Platform.isAndroid
+                    ? mLoginBlue.withOpacity(0.2)
+                    : Colors.transparent,
+              ),
             ),
-        ],
+            IgnorePointer(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4, right: 15),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(2),
+                      child: Image.asset(
+                        'lib/assets/m_login_button_white_icon.png',
+                        package: 'm_login_sdk',
+                        height: 33,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      text,
+                      style: const TextStyle(color: mLoginBlue, fontSize: 16),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (!enabled)
+              Container(
+                color: const Color.fromRGBO(210, 210, 210, 0.5),
+                width: double.infinity,
+                height: 40,
+              ),
+          ],
+        ),
       ),
     );
   }
