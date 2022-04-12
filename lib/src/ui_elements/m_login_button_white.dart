4e8d313c27f76bb147078cfc16aaee1e227528f7
Christian Koepke <koepke.christian@swm.de>
2022-04-12 15:21:58 +0200

    Fixed MLoginButtonWhite -> same structure as MLoginButton

--- lib/src/ui_elements/m_login_button_white.dart
+++ lib/src/ui_elements/m_login_button_white.dart
@@ -28,62 +28,65 @@ class MLoginButtonWhite extends StatelessWidget {
     return Semantics(
       button: true,
       label: semanticsLabel ?? text,
-      child: Material(
-        elevation: 2.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5),
-        ),
-        clipBehavior: Clip.hardEdge,
-        color: Colors.transparent,
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            Ink(
-              color: Colors.white,
-              width: double.infinity,
-              height: 40,
-              child: InkWell(
-                onTap: onPressed,
-                highlightColor: Platform.isAndroid
-                    ? mLoginBlue.withOpacity(0.1)
-                    : Colors.black.withOpacity(0.1),
-                splashColor: Platform.isAndroid
-                    ? mLoginBlue.withOpacity(0.2)
-                    : Colors.transparent,
-              ),
-            ),
-            IgnorePointer(
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 4, right: 15),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(2),
-                      child: Image.asset(
-                        'lib/assets/m_login_button_white_icon.png',
-                        package: 'm_login_sdk',
-                        height: 33,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      text,
-                      style: const TextStyle(color: mLoginBlue, fontSize: 16),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            if (!enabled)
-              Container(
-                color: const Color.fromRGBO(210, 210, 210, 0.5),
+      child: Padding(
+        padding: padding,
+        child: Material(
+          elevation: 2.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          clipBehavior: Clip.hardEdge,
+          color: Colors.transparent,
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Ink(
+                color: Colors.white,
                 width: double.infinity,
                 height: 40,
+                child: InkWell(
+                  onTap: onPressed,
+                  highlightColor: Platform.isAndroid
+                      ? mLoginBlue.withOpacity(0.1)
+                      : Colors.black.withOpacity(0.1),
+                  splashColor: Platform.isAndroid
+                      ? mLoginBlue.withOpacity(0.2)
+                      : Colors.transparent,
+                ),
               ),
-          ],
+              IgnorePointer(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 4, right: 15),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(2),
+                        child: Image.asset(
+                          'lib/assets/m_login_button_white_icon.png',
+                          package: 'm_login_sdk',
+                          height: 33,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        text,
+                        style: const TextStyle(color: mLoginBlue, fontSize: 16),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              if (!enabled)
+                Container(
+                  color: const Color.fromRGBO(210, 210, 210, 0.5),
+                  width: double.infinity,
+                  height: 40,
+                ),
+            ],
+          ),
         ),
       ),
     );
