013bf3d9651b52a8950d3061078d2ca7fcf2be97
Christian Koepke <koepke.christian@swm.de>
2022-06-10 13:58:04 +0200

    Revert "Updated example app to test changes"
    This reverts commit 83d6b56e1bb9a38dabc05bd92bb33e62c4954edd.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:m_login_sdk/m_login_sdk.dart';
-import 'package:rxdart/rxdart.dart';
 
 void main() {
   ///
@@ -41,9 +40,6 @@ class _ExampleLauncherPageState extends State<ExampleLauncherPage> {
   String paymentMethodsPageResultText = '';
   String driversLicenseResultText = '';
 
-  BehaviorSubject<String?> userName = BehaviorSubject.seeded(null);
-  BehaviorSubject<String?> userId = BehaviorSubject.seeded(null);
-
   bool ephemeral = false;
 
   ///
@@ -61,108 +57,60 @@ class _ExampleLauncherPageState extends State<ExampleLauncherPage> {
   );
 
   @override
-  initState() {
-    userName.listen((value) {
-      mLogin.prefilledUsername = value;
-    });
-    userId.listen((value) {
-      mLogin.loggedInMLoginUserId = value;
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('M-Login SDK'),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Row(
-                  children: [
-                    const Text('Ephemeral: '),
-                    Switch(
-                      value: ephemeral,
-                      onChanged: (value) {
-                        setState(() {
-                          ephemeral = value;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                MLoginButton(
-                  text: 'Mit M-Login anmelden',
-                  onPressed: _startLogin,
-                ),
-                Text(loginResultText),
-                ElevatedButton(
-                  onPressed: _startSignUp,
-                  child: const Text('Sign Up'),
-                ),
-                Text(signUpResultText),
-                ElevatedButton(
-                  onPressed: _openProfilePage,
-                  child: const Text('Profile Page'),
-                ),
-                Text(profilePageResultText),
-                ElevatedButton(
-                  onPressed: _openDriversLicenseVerificationPage,
-                  child: const Text('Drivers License Verification'),
-                ),
-                Text(driversLicenseResultText),
-                Container(
-                  color: Colors.grey,
-                  padding: const EdgeInsets.all(10),
-                  child: MLoginButtonWhite(
-                    text: 'Payment Methods Page',
-                    onPressed: _openPaymentMethodsPage,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                children: [
+                  const Text('Ephemeral: '),
+                  Switch(
+                    value: ephemeral,
+                    onChanged: (value) {
+                      setState(() {
+                        ephemeral = value;
+                      });
+                    },
                   ),
+                ],
+              ),
+              MLoginButton(
+                text: 'Mit M-Login anmelden',
+                onPressed: _startLogin,
+              ),
+              Text(loginResultText),
+              ElevatedButton(
+                onPressed: _startSignUp,
+                child: const Text('Sign Up'),
+              ),
+              Text(signUpResultText),
+              ElevatedButton(
+                onPressed: _openProfilePage,
+                child: const Text('Profile Page'),
+              ),
+              Text(profilePageResultText),
+              ElevatedButton(
+                onPressed: _openDriversLicenseVerificationPage,
+                child: const Text('Drivers License Verification'),
+              ),
+              Text(driversLicenseResultText),
+              Container(
+                color: Colors.grey,
+                padding: const EdgeInsets.all(10),
+                child: MLoginButtonWhite(
+                  text: 'Payment Methods Page',
+                  onPressed: _openPaymentMethodsPage,
                 ),
-                Text(paymentMethodsPageResultText),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      const Text(
-                        'To test prefilled user name function, login with valid account, '
-                        'enter user name and random user id and open service page (e.g. Profile Page)',
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextFormField(
-                        onChanged: (text) => userName.add(text),
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Username (e-Mail)',
-                        ),
-                        keyboardType: TextInputType.text,
-                        autofocus: true,
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextFormField(
-                        onChanged: (text) => userId.add(text),
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'User Id (may be random value)',
-                        ),
-                        keyboardType: TextInputType.text,
-                        autofocus: true,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+              Text(paymentMethodsPageResultText),
+            ],
           ),
         ),
       ),
@@ -173,9 +121,7 @@ class _ExampleLauncherPageState extends State<ExampleLauncherPage> {
     setState(() {
       loginResultText = 'Logging in...';
     });
-    final loginResult = await mLogin.login(
-      ephemeral: ephemeral,
-    );
+    final loginResult = await mLogin.login(ephemeral: ephemeral);
     setState(() {
       loginResult.process(
         (authCode, verifier) {
@@ -212,9 +158,7 @@ class _ExampleLauncherPageState extends State<ExampleLauncherPage> {
       profilePageResultText = 'Opening profile page...';
     });
 
-    final result = await mLogin.openPortalOverview(
-      ephemeral: ephemeral,
-    );
+    final result = await mLogin.openPortalOverview(ephemeral: ephemeral);
     setState(() {
       profilePageResultText = 'Profile was shown, result: $result';
     });
